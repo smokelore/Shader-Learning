80a679b01c81e2ec9dd79af661fbbfa95b538a85
Sebastian Monroy <sebastian.monroy@gmail.com>
2017-10-18 19:40:44 -0400

    chapter 8.3 BSC image effects

---
+++ Assets/_Scripts/Ch8/Ch8BSCImageEffect.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[ExecuteInEditMode] // makes the Screen Effect editable within the Editor without entering Play mode.
+public class Ch8BSCImageEffect : MonoBehaviour
+{
+    #region Variables
+    public Shader curShader;
+
+    [Range(0.0f, 2.0f)]
+    public float brightnessAmount = 1.0f;
+    [Range(0.0f, 2.0f)]
+    public float saturationAmount = 1.0f;
+    [Range(0.0f, 3.0f)]
+    public float contrastAmount = 1.0f;
+
+    private Material curMaterial;
+    #endregion
+
+    #region Properties
+    Material material
+    {
+        // material getter checks for a material, creates one if it doesn't find one.
+        get
+        {
+            if (curMaterial == null)
+            {
+                curMaterial = new Material(curShader);
+                curMaterial.hideFlags = HideFlags.HideAndDontSave;
+            }
+
+            return curMaterial;
+        }
+    }
+    #endregion
+
+    private void Start()
+    {
+        // check that the target platform supports image effects.
+        if (!SystemInfo.supportsImageEffects)
+        {
+            enabled = false;
+            return;
+        }
+
+        // script disables itself if image effects aren't supported.
+        if (curShader && !curShader.isSupported)
+        {
+            enabled = false;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        // access the render texture from the Unity Renderer.
+        if (curShader != null)
+        {
+            material.SetFloat("_BrightnessAmount", brightnessAmount);
+            material.SetFloat("_SaturationAmount", saturationAmount);
+            material.SetFloat("_ContrastAmount", contrastAmount);
+
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+
+    private void Update()
+    {
+        brightnessAmount = Mathf.Clamp(brightnessAmount, 0.0f, 2.0f);
+        saturationAmount = Mathf.Clamp(saturationAmount, 0.0f, 2.0f);
+        contrastAmount = Mathf.Clamp(contrastAmount, 0.0f, 3.0f);
+    }
+
+    private void OnDisable()
+    {
+        // clean up the curMaterial object we created in the material getter if this script is disabled.
+        if (curMaterial)
+        {
+            DestroyImmediate(curMaterial);
+        }
+    }
+}
---
+++ Assets/_Shaders/Ch8/Ch8BSCImageEffect.shader
@@ -0,0 +1,69 @@
+﻿Shader "CookbookShaders/Ch08/BSCImageEffect.shader"
+{
+	Properties
+	{
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_BrightnessAmount ("Brightness Amount", Range(0.0, 1.0)) = 1.0
+		_SaturationAmount ("Saturation Amount", Range(0.0, 1.0)) = 1.0
+		_ContrastAmount ("Contrast Amount", Range(0.0, 1.0)) = 1.0
+	}
+
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			#pragma fragmentoption ARB_precision_hint_fastest
+			#include "UnityCG.cginc"
+
+			// declare corresponding CGPROGRAM variables
+			uniform sampler2D _MainTex;
+			fixed _BrightnessAmount;
+			fixed _SaturationAmount;
+			fixed _ContrastAmount;
+
+			float3 BrightnessSaturationContrast(float3 color, float brightness, float saturation, float contrast)
+			{
+				// adjust these values to adjust R, G, B colors separately
+				float avgLumR = 0.5;
+				float avgLumG = 0.5;
+				float avgLumB = 0.5;
+
+				// luminance coefficient for getting luminance from the image
+				float3 luminanceCoeff = float3(0.2125, 0.7154, 0.0721);
+
+				// Brightness calculation
+				float3 avgLum = float3(avgLumR, avgLumG, avgLumB);
+				float3 brightnessColor = color * brightness;
+				float intensityf = dot(brightnessColor, luminanceCoeff);
+				float3 intensity = float3(intensityf, intensityf, intensityf);
+
+				// Saturation calculation
+				float3 saturationColor = lerp(intensity, brightnessColor, saturation);
+
+				// Contrast calculation
+				float3 contrastColor = lerp(avgLum, saturationColor, contrast);
+
+				return contrastColor;
+			}
+
+			fixed4 frag(v2f_img i) : COLOR
+			{
+				// Get the colors from the RenderTexture and the UVs from the v2f_img struct
+				fixed4 renderTex = tex2D(_MainTex, i.uv);
+
+				// Apply the Brightness, Saturation, and Contrast calculations
+				renderTex.rgb = BrightnessSaturationContrast(renderTex.rgb,
+															_BrightnessAmount,
+															_SaturationAmount,
+															_ContrastAmount);
+	
+				return renderTex;
+			}
+
+			ENDCG
+		}
+	}
+}
